d4c18667ff185039a0f9344d187bcf0fe3d50946
Ashwin <ashwindeepak1997@gmail.com>
2024-11-08 09:32:07 +0530

    ver2

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
 const path = require("path");
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const { log } = require("console");
+const { generateMessage, generateLocationMessage } = require("./utils/messages");
 
+const app = express();
 
+const server = http.createServer(app); //http server that wraps the express app
+const io = socketio(server); //initializing socket io
 
-const app = express()
-
-const server = http.createServer(app) //http server that wraps the express app
-const io = socketio(server) //initializing socket io
-
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 //serving the static files
-const publicPath = path.join(__dirname, './public')
-app.use(express.static(publicPath))
+const publicPath = path.join(__dirname, "./public");
+app.use(express.static(publicPath));
 
 let count = 0;
 
 // sets up an event listener for new client connections(every refresh creates a new connection)
-io.on('connection', (socket) => { //socket is an object
-   console.log('new socket connection');
-   socket.emit('countUpdated', count) //send inital count to the clients
-   socket.on('increment', () => { //event listener for increment event
-       count++;
-       io.emit('countUpdated', count) //send updated count to all clients || if u put socket.emit only send to single client
-   })
-})
+// io.on('connection', (socket) => { //socket is an object
+//    console.log('new socket connection');
+//    socket.emit('countUpdated', count) //send inital count to the clients
+//    socket.on('increment', () => { //event listener for increment event
+//        count++;
+//        io.emit('countUpdated', count) //send updated count to all clients || if u put socket.emit only send to single client
+//    })
+// })
 
+// challenge
+io.on("connection", (socket) => {
+  console.log("new socket connection");
 
+  // socket.emit("message", "Welcome to the chat app");
 
+  //setting up timestamp for messages so commented above code
+  socket.emit("message", generateMessage("Welcome to the chat app"))
+  socket.broadcast.emit("message", generateMessage("A new user has joined")); // send to all clients except the one who joined
+  
+  socket.on("sendMessage", (message, callback) => {
+    io.emit("message", generateMessage(message)); //send to all clients
+    callback('Delivered!'); //this is third argument in scoket.emit in chatjs
+  });
+
+  // listener for sending the location
+  socket.on("sendLocation", (coords, callback) => {
+    io.emit(
+      "locationMessage",
+      generateLocationMessage(`https://google.com/maps?/q=${coords.latitude},${coords.longitude}`)
+    ); //${coords.latitude}, ${coords.longitude} `
+    callback();
+  });
+
+  //notifying when a user leaves
+  socket.on("disconnect", () => {
+    io.emit("message", generateMessage("A user has left")); //send to all clients still connected
+  });
+});
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,92 @@
-
-
-    //io() //creates a socket connection from client side. This call initiates the connection with the server side.
+//io() //creates a socket connection from client side. This call initiates the connection with the server side.
 
 const socket = io();
-socket.on("countUpdated",(count)=>{
-    console.log("the count has been updated",count);
-}) //listening to the event countUpdated from the server side and updating the count on the client side.
 
-//the name in emit and on should be same
+// socket.on("countUpdated",(count)=>{
+//     console.log("the count has been updated",count);
+// }) //listening to the event countUpdated from the server side and updating the count on the client side.
 
-document.querySelector("#increment").addEventListener("click",()=>{
-    console.log("clicked");
-    socket.emit("increment"); //from client side to server side
+// //the name in emit and on should be same
+
+// document.querySelector("#increment").addEventListener("click",()=>{
+//     console.log("clicked");
+//     socket.emit("increment"); //from client side to server side
+
+// })
+
+//elements for form and button states
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+//templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML;
+//challenge to message
+/* socket.on("message", (message) => {
+  console.log("message received :", message);
+}); */
+socket.on("message",(message)=>{
+  console.log(message);
+  const html = Mustache.render(messageTemplate,{
+    message : message.text,
+    createdAt : moment(message.createdAt).format('h:mm a') //since the time is unreadable - momentjs is used
     
+  })
+  $messages.insertAdjacentHTML("beforeend", html);
+  
 })
+
+//listener for location
+socket.on("locationMessage",(message)=>{
+  console.log(message);
+  const html = Mustache.render(locationMessageTemplate,{
+    url : message.url,
+    createdAt : moment(message.createdAt).format('h:mm a') 
+  })
+  $messages.insertAdjacentHTML("beforeend", html); 
+})
+
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled"); //disabling the button when the form is submitted
+  
+  const message = e.target.elements.message.value; //gets the value of the input field with name = "message" || this is used instead of queryselector
+  
+  socket.emit("sendMessage", message,(error)=>{ 
+    
+    //enable submit button
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if(error){
+      return console.log(error);
+    }
+    
+    console.log('Message Delivered!');
+    
+  }); //message is send to the callback in "socket.on('sendMessage')"
+});
+
+$sendLocationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  $sendLocationButton.setAttribute("disabled", "disabled"); //disabling the button when the form is submitted
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    },()=>{
+      $sendLocationButton.removeAttribute("disabled");
+      console.log("Location shared!");
+    });
+  });
+});
